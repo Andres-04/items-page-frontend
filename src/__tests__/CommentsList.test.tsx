6e12b05f88d40280be1a5ed2f95731b8d676d632
andres.ortegon <ivanandres.ortegon@bbva.com>
2025-07-21 11:21:08 -0500

    Feature: Add first test with jest

---
+++ src/__tests__/CommentsList.test.tsx
@@ -0,0 +1,37 @@
+import {render, screen} from '@testing-library/react';
+import CommentsList from '@/components/CommentsList';
+
+
+describe('CommentsList', () => {
+  it('should render one comment', () => {
+    const reviews = [
+      {
+        'date': '2024-07-12',
+        'comment': 'Excelente producto, me llegó muy rápido.',
+        'score': 4.5,
+        'images': []
+      }
+    ];
+    render(<CommentsList reviews={reviews}/>);
+
+    expect(screen.getByText('Excelente producto, me llegó muy rápido.')).toBeInTheDocument();
+    expect(screen.getByText('2024-07-12')).toBeInTheDocument();
+  });
+
+  it('should render default values', () => {
+    const reviews = [
+      {
+        'date': '',
+        'comment': '',
+        'score': 0.0,
+        'images': []
+      }
+    ];
+    render(<CommentsList reviews={reviews}/>);
+    expect(screen.getByText('2000-01-01')).toBeInTheDocument();
+  });
+});
+
+
+
+
